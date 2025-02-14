941193fcd0fcca34862b420c9cc9325d69dc8355
Nick <nikita_korneev101@mail.ru>
2025-02-14 16:33:00 +0300

    add startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 const Page = async ({
 	searchParams,
@@ -6,16 +7,67 @@ const Page = async ({
 	searchParams: Promise<{ query?: string }>;
 }) => {
 	const query = (await searchParams).query;
+
+	const posts = [
+		{
+			_createdAt: new Date(),
+			views: 55,
+			author: { _id: 1, name: "Victor" },
+			_id: 1,
+			description: "This is description",
+			image: "https://placehold.co/400",
+			category: "Robots",
+			title: "We Robots",
+		},
+		{
+			_createdAt: new Date(),
+			views: 55,
+			author: { _id: 1, name: "Victor" },
+			_id: 1,
+			description: "This is description",
+			image: "https://placehold.co/400",
+			category: "Robots",
+			title: "We Robots",
+		},
+		{
+			_createdAt: new Date(),
+			views: 55,
+			author: { _id: 1, name: "Victor" },
+			_id: 1,
+			description: "This is description",
+			image: "https://placehold.co/400",
+			category: "Robots",
+			title: "We Robots",
+		},
+	];
 	return (
-		<div className="pink_container">
-			<h1 className="heading">
-				Pitch Your Startup, <br /> Connect With Enterpreteurs
-			</h1>
-			<p className="sub-heading !max-w-3xl">
-				Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-			</p>
-			<SearchForm query={query} />
-		</div>
+		<>
+			<section className="pink_container">
+				<h1 className="heading">
+					Pitch Your Startup, <br /> Connect With Enterpreteurs
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+				</p>
+				<SearchForm query={query} />
+			</section>
+
+			<section className="section_container">
+				<p className="text-30-semibold">
+					{query ? "Results for " + query : "Trending"}
+				</p>
+
+				<ul className="mt-7 card_grid">
+					{posts.length > 0 ? (
+						posts.map((post) => (
+							<StartupCard post={post} key={post._id} />
+						))
+					) : (
+						<p className="no-results">No startups found</p>
+					)}
+				</ul>
+			</section>
+		</>
 	);
 };
 
